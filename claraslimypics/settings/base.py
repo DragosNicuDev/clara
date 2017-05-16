f44005db43f51ef67080dd72829ff97aae06a723
Dragos Nicu <contact@dragosnicu.com>
2017-05-16 17:40:05 +0100

    Settings, static files and settings

--- claraslimypics/settings/base.py
+++ claraslimypics/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'claraslimypics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
--- claraslimypics/settings/development.py
+++ claraslimypics/settings/development.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 from .base import *
+
+INSTALLED_APPS += [
+    # required by allauth
+    'allauth',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+]
+
+SITE_ID = 1
